fe09746b37f95ac4f556ed4aca8d3a0c456ed423
Daniel Kalmucki <danielkalmucki2@gmail.com>
2024-08-24 00:44:07 +0200

    Simple moving object

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#include <algorithm>
+#include <cstdint>
+#include <cstring>
+
 #include "driver/gpio.h"
 #include "esp_event.h"
 #include "esp_system.h"
-#include "freertos/FreeRTOS.h"
-
-#include "ssd1306.h"
 #include "font8x8_basic.h"
+#include "freertos/FreeRTOS.h"
+#include "ssd1306.h"
 
 struct MovementActionParams
 {
@@ -33,29 +36,46 @@ void handleInputs(void* movement_action_params)
             params->move_right = 1;
         else
             params->move_right = 0;
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 };
-
 
 extern "C" void app_main()
 {
     printf("Hello world!\n");
     SSD1306_t dev;
-	int center, top, bottom;
-	char lineChar[20];
+    int top;
+    char lineChar[20];
     i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
     ssd1306_init(&dev, 128, 64);
     ssd1306_clear_screen(&dev, false);
-	ssd1306_contrast(&dev, 0xff);
-	ssd1306_display_text_x3(&dev, 0, "Hello", 5, false);
+    ssd1306_contrast(&dev, 0xff);
+
+    top = 2;
+    std::uint8_t image[24] = {0};
+    for (int font = 0x39; font > 0x30; font--)
+    {
+        std::memset(image, 0, sizeof(image));
+        ssd1306_display_image(&dev, top + 1, (7 * 8 - 1), image, 8);
+        std::memcpy(image, font8x8_basic_tr[font], 8);
+        if (dev._flip) ssd1306_flip(image, 8);
+        ssd1306_display_image(&dev, top + 1, (7 * 8 - 1), image, 8);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+    ssd1306_clear_screen(&dev, false);
 
     MovementActionParams params;
     xTaskCreate(handleInputs, "Input handler", 8192, &params, 5, NULL);
 
+    int position_x = 0;
+    std::uint8_t ship_image[1] = {1};
     while (1)
     {
-        printf("left: %d, right %d\n", params.move_left_, params.move_right);
+        ssd1306_clear_screen(&dev, false);
+        position_x = std::max(0, std::min(7, position_x + params.move_right - params.move_left_));
+        ssd1306_display_image(&dev, position_x, 0, ship_image, 1);
+
+        printf("left: %d, right %d, pos %d\n", params.move_left_, params.move_right, position_x);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     };
 }
