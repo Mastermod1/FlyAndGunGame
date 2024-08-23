ddf7c03aea63a650ef6eb09b6c3aa082a8db5a81
Daniel Kalmucki <danielkalmucki2@gmail.com>
2024-08-24 00:16:58 +0200

    Simple I2C oled display handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include "esp_event.h"
 #include "esp_system.h"
 #include "freertos/FreeRTOS.h"
-#include <U8g2lib.h>
+
+#include "ssd1306.h"
+#include "font8x8_basic.h"
 
 struct MovementActionParams
 {
@@ -39,6 +41,15 @@ void handleInputs(void* movement_action_params)
 extern "C" void app_main()
 {
     printf("Hello world!\n");
+    SSD1306_t dev;
+	int center, top, bottom;
+	char lineChar[20];
+    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
+    ssd1306_init(&dev, 128, 64);
+    ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text_x3(&dev, 0, "Hello", 5, false);
+
     MovementActionParams params;
     xTaskCreate(handleInputs, "Input handler", 8192, &params, 5, NULL);
 
